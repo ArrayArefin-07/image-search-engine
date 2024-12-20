800fcaba750de4d872c7bd8c3ed0941cd752297a
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-12-20 07:15:22 +0600

    define variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResults = document.getElementById("search-results");
+const searchBtn = document.getElementById("show-more-btn");
