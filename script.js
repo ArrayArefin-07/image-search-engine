801c358bfaf3098e7872986c98e567d8c6b6b016
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-12-20 08:30:22 +0600

    displayed show More button

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let page = 1;
 
 async function searchImages() {
   keyword = searchBox.value;
-  const url =`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+  const url =`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=15`;
 
   const response = await fetch(url);
   const data = await response.json();
@@ -28,6 +28,7 @@ async function searchImages() {
     imageLink.appendChild(image);
     searchResults.appendChild(imageLink);
   })
+  searchBtn.style.display = "block";
 
 }
 
