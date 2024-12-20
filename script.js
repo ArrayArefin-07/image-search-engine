ae114648516c8d33a7e58e92e622c8b3cbca79ae
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-12-20 07:59:01 +0600

    work with api calling
    added functionality after searching image

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
+const accessKey = "zEvVrMzy1tpNWz4scPMsvugucB0hX45FSsQ1ZAQqJ24";
+
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResults = document.getElementById("search-results");
 const searchBtn = document.getElementById("show-more-btn");
+
+
+let keyword = "";
+let page = 1;   
+
+async function searchImages() {
+  keyword = searchBox.value;
+  const url =`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+
+  results.map((results => {
+    const image = document.createElement("img");
+    image.src = results.urls.regular;
+  }))
+
+}
+
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  page = 1;
+  searchImages();
+});
